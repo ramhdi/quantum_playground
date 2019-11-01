87215f62994a4f9bba6b95124afe2ccf567b43bb
Rama Rahardi <rama.orion@gmail.com>
2019-11-01 23:48:22 +0700

    Entanglement in q#

---
+++ qsharp/quantum_bell/host.py
@@ -0,0 +1,9 @@
+import qsharp
+from qsharp import Result
+from Quantum.Bell import TestBellState
+
+initials = {Result.Zero, Result.One}
+for i in initials:
+    res = TestBellState.simulate(count=10000, initial=i)
+    (num_zeros, num_ones, agree) = res
+    print(f'Init:{i: <4} 0s={num_zeros: <4} 1s={num_ones: <4} agree={agree: <4}')
